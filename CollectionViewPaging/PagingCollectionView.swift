4bf3d84004993cf7219ef6b8fb2de677c1cb8790
Pablo Alcalde <pablo.alcalde@cabify.com>
2017-07-03 16:20:06 +0200

    Created PagingCollectionView

--- CollectionViewPaging/PagingCollectionView.swift
+++ CollectionViewPaging/PagingCollectionView.swift
@@ -1,6 +1,102 @@
 import Foundation
 import UIKit
 
-class PagingCollectionView: UICollectionView {
+class PagingCollectionView: UICollectionView, UICollectionViewDelegate {
+    private let pagingScrollView = UIScrollView()
+
+    var pageSize: CGFloat = 0 {
+        didSet {
+            didResize()
+        }
+    }
+
+    // 1 is no effect at all, 0.1 is the biggest effect possible
+    // defaults to 0.7
+    var scalePagingEffect: CGFloat = 0.7 {
+        didSet {
+            scalePagingEffect = min(max(scalePagingEffect, 0.1), 1.0)
+        }
+    }
+
+    private var flowLayout: UICollectionViewFlowLayout {
+        return collectionViewLayout as! UICollectionViewFlowLayout
+    }
+
+    init(pageSize: CGFloat, scalePagingEffect: CGFloat = 0) {
+        self.pageSize = pageSize
+        super.init(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
+        configure()
+    }
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configure()
+    }
+
+    private func configure() {
+        delegate = self
+
+        isPagingEnabled = false
+
+        flowLayout.minimumInteritemSpacing = 0
+        flowLayout.minimumLineSpacing = 0
+
+        didResize()
+
+        addSubview(pagingScrollView)
+
+        pagingScrollView.translatesAutoresizingMaskIntoConstraints = false
+        pagingScrollView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        pagingScrollView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        pagingScrollView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        pagingScrollView.widthAnchor.constraint(equalToConstant: pageSize).isActive = true
+
+        pagingScrollView.isPagingEnabled = true
+        pagingScrollView.delegate = self
+        pagingScrollView.isUserInteractionEnabled = false
+
+        addGestureRecognizer(pagingScrollView.panGestureRecognizer)
+        panGestureRecognizer.isEnabled = false
+
+        contentMode = .redraw
+    }
+
+    override func draw(_ rect: CGRect) {
+        didResize()
+    }
+
+    private func didResize() {
+        flowLayout.itemSize = CGSize(width: pageSize, height: frame.height)
+        let padding = (frame.width-pageSize)/2
+        flowLayout.sectionInset = UIEdgeInsets(top: 0, left: padding, bottom: 0, right: padding)
+        pagingScrollView.contentSize = CGSize(width: pageSize * 5, height: frame.height)
+        flowLayout.scrollDirection = UICollectionViewScrollDirection.horizontal
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if (scrollView == pagingScrollView) { //ignore collection view scrolling callbacks
+            contentOffset = pagingScrollView.contentOffset
+
+            let rows = indexPathsForVisibleItems
+            for indexPath in rows {
+                applyTransformToCell(cell: cellForItem(at: indexPath)!, indexPath: indexPath)
+            }
+        }
+    }
+
+    private func applyTransformToCell(cell: UICollectionViewCell, indexPath: IndexPath) {
+        let scrollCenter = contentOffset.x + (frame.width/2)
+
+        let collectionViewWidth = frame.width
+        let offSetX = abs(scrollCenter - cell.center.x)
+        let percetangeTransformToApply = (collectionViewWidth - offSetX) / collectionViewWidth
+
+        let transformScaled = scalePagingEffect + (percetangeTransformToApply * (1.0 - scalePagingEffect))
+
+        cell.contentView.layer.sublayerTransform = CATransform3DMakeScale(transformScaled, transformScaled, 1)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("selected cell \(indexPath.row)")
+    }
 }
--- CollectionViewPaging/ViewController.swift
+++ CollectionViewPaging/ViewController.swift
@@ -8,73 +8,26 @@
 
  import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class ViewController: UIViewController, UICollectionViewDataSource {
 
-    @IBOutlet weak var collectionView: UICollectionView!
+    let collectionView = PagingCollectionView.init(pageSize: 250)
 
     private let scrollView = UIScrollView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.delegate = self
+
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = UIColor.clear
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        collectionView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        collectionView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+//        collectionView.delegate = self
         collectionView.register(UINib.init(nibName: "Cell", bundle: nil), forCellWithReuseIdentifier: "cell")
         collectionView.dataSource = self
-
-        let layout: UICollectionViewFlowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-
-        layout.scrollDirection = UICollectionViewScrollDirection.horizontal
-        collectionView.isPagingEnabled = false
-
-        layout.minimumInteritemSpacing = 0
-        layout.minimumLineSpacing = 0
-
-        let windowWidth: CGFloat = 374
-        let pageSize: CGFloat = 250
-
-        layout.itemSize = CGSize(width: pageSize, height: collectionView.frame.height)
-        let padding = (windowWidth-pageSize)/2
-
-        layout.sectionInset = UIEdgeInsets(top: 0, left: padding, bottom: 0, right: padding)
-
-        view.addSubview(scrollView)
-        scrollView.contentSize = CGSize(width: pageSize * 5, height: collectionView.frame.height)
-
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.topAnchor.constraint(equalTo: collectionView.topAnchor).isActive = true
-        scrollView.bottomAnchor.constraint(equalTo: collectionView.bottomAnchor).isActive = true
-        scrollView.leftAnchor.constraint(equalTo: collectionView.leftAnchor).isActive = true
-        scrollView.widthAnchor.constraint(equalToConstant: pageSize).isActive = true
-
-        scrollView.isPagingEnabled = true
-        scrollView.delegate = self
-        scrollView.isUserInteractionEnabled = false
-
-        collectionView.addGestureRecognizer(scrollView.panGestureRecognizer)
-        collectionView.panGestureRecognizer.isEnabled = false
-    }
-
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if (scrollView == self.scrollView) { //ignore collection view scrolling callbacks
-            collectionView.contentOffset = scrollView.contentOffset
-
-            let rows = self.collectionView.indexPathsForVisibleItems
-            for indexPath in rows {
-                applyTransformToCell(cell: collectionView.cellForItem(at: indexPath)!, indexPath: indexPath)
-            }
-        }
-    }
-
-    private func applyTransformToCell(cell: UICollectionViewCell, indexPath: IndexPath) {
-        let scrollCenter = collectionView.contentOffset.x + (collectionView.frame.width/2)
-
-        let collectionViewWidth = collectionView.frame.width
-        let offSetX = abs(scrollCenter - cell.center.x)
-        let percetangeTransformToApply = (collectionViewWidth - offSetX) / collectionViewWidth
-
-        let scale: CGFloat = 0.7
-        let transformScaled = scale + (percetangeTransformToApply * (1.0-scale))
-        
-        cell.contentView.layer.sublayerTransform = CATransform3DMakeScale(transformScaled, transformScaled, 1)
     }
 
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -83,7 +36,6 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
 
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! Cell
-        cell.image.image = UIImage.init(named: "vehicle.png")
         return cell
     }
     
